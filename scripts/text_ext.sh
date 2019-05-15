089a8b718c9897007fc61e8103f30adacedb7101
mybayern1974 <mybayern1974@hotmail.com>
2019-05-15 13:18:55 +0800

    Delete text_ext.sh

--- scripts/text_ext.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo sh -c 'java -jar /opt/hello-karyon-rxnetty/hello-karyon-rxnetty*.jar > /var/log/hello-karyon-rxnetty/hello1.log 2>&1 &'
-sleep 1m
-sudo sh -c 'java -jar /opt/hello-karyon-rxnetty/hello-karyon-rxnetty*.jar > /var/log/hello-karyon-rxnetty/hello2.log 2>&1 &'
-sleep 1m
-sudo sh -c 'java -jar /opt/hello-karyon-rxnetty/hello-karyon-rxnetty*.jar > /var/log/hello-karyon-rxnetty/hello3.log 2>&1 &'
-sleep 1m
-sudo sh -c 'java -jar /opt/hello-karyon-rxnetty/hello-karyon-rxnetty*.jar > /var/log/hello-karyon-rxnetty/hello4.log 2>&1 &'
